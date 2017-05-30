d25e2902e74ea5f90c7169830033189f2158296f
Jon Jaffe <jonjaffe@Jons-MacBook-Pro.local>
2017-05-30 12:34:59 -0700

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './lib/entry.js',
+  output: {
+    filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
